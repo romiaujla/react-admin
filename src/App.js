6fa0e47e4ae26f92480fdffd80758337300d3b2d
Raman Aujla <raujla@developrise.com>
2023-03-04 19:04:21 -0500

    added the top bar to the app js, and moved the location of the global folders to be scope one step up, under src directly and not be nested under dashboard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "../src/scenes/global/TopBar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -8,7 +9,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+            <TopBar></TopBar>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
--- src/scenes/dashboard/gobal/TopBar.jsx
+++
@@ -1,5 +0,0 @@
-const TopBar = () => {
-    return <div>TopBar</div>
-}
-
-export default TopBar;
